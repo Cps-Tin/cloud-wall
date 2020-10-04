b47a1f9950486dfb00732a43fbd5eb85665cbf12
Cai Peishen <peishen.cai@foxmail.com>
2020-10-04 21:51:27 +0800

    简单修改

--- src/main/java/cn/cw/Application.java
+++ src/main/java/cn/cw/Application.java
@@ -4,6 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.event.EventPublishingRunListener;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @MapperScan("cn.cw.dao")  //扫描dao层
